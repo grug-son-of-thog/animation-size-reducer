cd39d4c672146bbe287f852340c414bc657cdd02
Mark <mark.swam@gmail.com>
2023-12-18 22:59:53 -0600

    gitignore

--- convert.bat
+++ convert.bat
@@ -8,7 +8,7 @@ for %%A in (input\*.gif) do (
         for /f "delims=" %%a in ('bin\ffprobe.exe -v error -select_streams v:0 -show_entries stream^=height -of default^=noprint_wrappers^=1:nokey^=1 "%%A"') do (
             set "resolution=%%a"
         )
-        bin\gifski.exe --fps 24 -H !resolution! -o "output\%%~nA.gif" "%%~dpnA"\*
+        bin\gifski.exe --fps 20 -H !resolution! -o "output\%%~nA.gif" "%%~dpnA"\*
         rmdir /q /s "%%~dpnA"
     ) else (
         echo %%~A does not exist.
